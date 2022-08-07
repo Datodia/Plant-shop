7b9bee7622733a83875bc024876ffa57904dc167
Datodia <dato.diasamidze.02@gmail.com>
2022-08-07 14:51:55 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const person = {
+    name: "Dato",
+    surname: "Diasamidze",
+    age: 20,
+    isMarried: false,
+    city: "Tbilisi",
+    programingLang: "JS",
+    sex: "Male",
+    working: false,
+    height: 186,
+    hasdog: true
+}
+
+console.log(person.name)
+console.log(person.surname)
+console.log(person.age)
+console.log(person.isMarried)
+console.log(person.city)
+console.log(person.programingLang)
+console.log(person.sex)
+console.log(person.working)
+console.log(person.height)
+console.log(person.hasdog)
